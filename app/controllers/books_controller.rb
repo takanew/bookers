ad7b06d97329e51daad67555c26bbc648945cc73
takanew <145203231+takanew@users.noreply.github.com>
2023-11-01 04:43:41 +0000

    [Finish]create作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,25 @@
 class BooksController < ApplicationController
 
   def index
-    @book = Book.new 
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def show
   end
 
   def edit
+  end
+
+
+private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
