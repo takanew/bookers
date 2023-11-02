c70c8efe0e8f085fc04dfee96fcf4a8c26a59866
takanew <145203231+takanew@users.noreply.github.com>
2023-11-02 07:00:26 +0000

    [Finish]update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,12 +10,19 @@ class BooksController < ApplicationController
     @book.save
     redirect_to book_path(@book.id)
   end
-
+  
   def show
     @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
 
